f5529f3e8914e98f20eff4d85fd6283da54cf567
leuhsouza <leuhsouza@gmail.com>
2024-01-18 16:26:21 -0300

    test: add test bubble sort

---
+++ spec/lib/bubble_sort_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'hero'
+
+describe 'bubble_sort' do it 'Deve retornar a lista ordenada' do
+  ordarray = bubble_sort([64, 34, 25, 12, 22, 11, 90])
+  expect(ordarray).to eq([11,12,22,25,34,64,90])
+  end
+end
