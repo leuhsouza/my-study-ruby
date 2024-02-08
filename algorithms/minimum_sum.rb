01657bf732d40d3ef0f48d6f856990c703c2db4d
leuhsouza <leuhsouza@gmail.com>
2024-02-08 14:53:59 -0300

    feat: add min sum alg

---
+++ algorithms/minimum_sum.rb
@@ -0,0 +1,11 @@
+def min_sum (arr)
+    min_val = arr.min
+
+    return min_val * ((arr.length) -1)
+end
+
+# result = min_sum([7, 2, 3, 4, 5, 6])
+# puts "#{result}"
+
+# result = min_sum([2, 4, 1, 3])
+# puts "#{result}"
